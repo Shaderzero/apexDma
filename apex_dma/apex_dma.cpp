0b8ff139d6d692f8de2722e43421910b623becb7
ShaderZero <shaderzero@gmail.com>
2023-07-27 02:17:21 +0300

    change client offset

--- apex_dma/apex_dma.cpp
+++ apex_dma/apex_dma.cpp
@@ -709,12 +709,12 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	const char* cl_proc = "client_ap.exe";
+	const char* cl_proc = "razer.exe";
 	const char* ap_proc = "R5Apex.exe";
 	//const char* ap_proc = "EasyAntiCheat_launcher.exe";
 
 	//Client "add" offset
-	uint64_t add_off = 0x3f880;
+	uint64_t add_off = 0x3f870;
 
 	std::thread aimbot_thr;
 	std::thread esp_thr;
